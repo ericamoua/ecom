a9e2e009e0aa6be4a26e5d08b33b7cf9b6210f45
EricaMoua <erica.moua568@gmail.com>
2024-05-06 08:50:13 -0400

    added nav amd footer

--- javascript.js
+++ javascript.js
@@ -2,3 +2,12 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
   });
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, {
+        fullWidth: true,
+    });
+  });
+  
